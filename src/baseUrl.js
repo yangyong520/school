85bd5b64ee4263e81fdf5993482e5423eeb7c5ec
yangyong <1270201010@qq.com>
2019-07-31 14:22:36 +0800

    add

---
+++ src/baseUrl.js
@@ -0,0 +1,8 @@
+// export const baseUrl = 'apis'
+// export const baseUrl = 'http://192.168.1.181:8082' // 韩
+// export const baseUrl = 'http://101.207.139.80:8082' // 线上
+export const baseUrl = 'http://192.168.20.158:8082' // 廖
+// export const baseUrl = 'http://192.168.20.180:8082' // 敏敏
+// target: 'http://192.168.20.180:8082',  // 敏敏
+// target: 'http://192.168.20.128:8082'  // 韩
+// target: 'http://192.168.20.50:8082',   // 赖兴
